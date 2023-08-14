04f1cb14608501c400b7ebfad40d44ed229b3c89
Aditya Dhage <adhage93@gmail.com>
2023-08-14 11:15:24 -0700

    Love Calculator JS

---
+++ 14-introduction-to-javascript-es6/loveCalculator.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+let name1 = prompt("What's your name? ");
+let name2 = prompt("What's their name? ");
+
+let loveScore = Math.random() * 100;
+console.log(`${name1} and ${name2} scored ${loveScore} on our calculator!!!`);
