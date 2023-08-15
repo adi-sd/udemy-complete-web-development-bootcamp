b3d55b739cd8893c1f866e08626d133f0c773de5
Aditya Dhage <adhage93@gmail.com>
2023-08-15 11:36:42 -0700

    Prompt Sync JS

--- 14-introduction-to-javascript-es6/loveCalculator.js
+++ 14-introduction-to-javascript-es6/loveCalculator.js
@@ -5,3 +5,5 @@ let name2 = prompt("What's their name? ");
 
 let loveScore = Math.random() * 100;
 console.log(`${name1} and ${name2} scored ${loveScore} on our calculator!!!`);
+
+// prompt-sync is better for taking input from console node js
