38b21b3aaaaa8ae734444ca74dda57437385e956
Aditya Dhage <adhage93@gmail.com>
2023-08-17 10:16:48 -0700

    New Commit

--- 14-introduction-to-javascript-es6/loveCalculator.js
+++ 14-introduction-to-javascript-es6/loveCalculator.js
@@ -7,3 +7,5 @@ let loveScore = Math.random() * 100;
 console.log(`${name1} and ${name2} scored ${loveScore} on our calculator!!!`);
 
 // prompt-sync is better for taking input from console node js
+
+// new commit
